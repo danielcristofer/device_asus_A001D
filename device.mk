004bb0a474e870f6870047086b2ef568af6341bd
danielcristofer <danielcristofer@gmail.com>
2026-07-14 23:18:32 +0000

    A001D: fix wifi driver path, security patch mismatch, fingerprint sepolicy for gx_fpd_device and wcnss_service

--- device.mk
+++ device.mk
@@ -480,4 +480,3 @@ PRODUCT_COPY_FILES += \
 		$(LOCAL_PATH)/configs/wifi/WCNSS_wlan_dictionary.dat:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_wlan_dictionary.dat
 
 PRODUCT_COPY_FILES += \
-		$(LOCAL_PATH)/prebuilt/pronto_wlan.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules/wlan.ko
